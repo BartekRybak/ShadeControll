c475a39ddb7bf5012f174a99c77fac8a3d4ecce2
Bartek Rybak <bartekrybakofficial@gmail.com>
2021-03-21 16:24:38 +0100

    podstawowe dzialania

--- ShadeControll/Program.cs
+++ ShadeControll/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using System.Threading;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
 
 namespace ShadeControll
 {
     class Program
     {
-        static void Main(string[] args)
+        static TelegramClient Client;
+        static ITelegramBotClient botClient;
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Client = new TelegramClient("1774037430:AAHnjjeOUNvn-ZpyCCo_6mIhztp_GkagsVg");
+
+            while(!Client.Connect())
+            {
+                Console.WriteLine("Connecting..");
+                Thread.Sleep(1000);
+            }
+            Client.SendMessage("Połączono z Komputerem");
+            Client.NewMessage += Client_NewMessage;
+
+            while (true) { Thread.Sleep(1000); }
+        }
+
+        private static void Client_NewMessage(string message)
+        {
+            switch (message)
+            {
+                case "/help":
+                    Client.SendMessage("/help /shutdown /logout");
+                    break;
+
+                case "/shutdown":
+                    Client.SendMessage("Wyłączanie Komputera");
+                    Shutdown();
+                    break;
+
+                case "/logout":
+                    Client.SendMessage("Wylogowywanie");
+                    LogOut();
+                    break;
+            }
+        }
+
+        private static void Shutdown()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("WYŁĄCZANIE");
+            Process.Start("shutdown", "/s /t 0");
+        }
+
+        private static void LogOut()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("WYLOGOWYWANIE");
+            Process.Start("shutdown","/l");
         }
     }
 }
---
+++ ShadeControll/TelegramClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace ShadeControll
+{
+    class TelegramClient
+    {
+        const string MY_ID_CHAT = "1623222858";
+
+        static ITelegramBotClient botClient;
+        static string Token = string.Empty;
+
+        public delegate void NewMessageDelegate(string message);
+        public event NewMessageDelegate NewMessage;
+
+        public TelegramClient(string token)
+        {
+            Token = token;
+        }
+
+        public void CloseConnection()
+        {
+            botClient.StopReceiving();
+        }
+
+        public bool Connect()
+        {
+           try
+           {
+                botClient = new TelegramBotClient(Token);
+                var me = botClient.GetMeAsync().Result;
+                botClient.OnMessage += Bot_OnMessage;
+                botClient.StartReceiving();
+                return true;
+           }
+           catch
+           {
+                return false;
+            }
+        }
+
+        public async void SendMessage(string _text)
+        {
+            await botClient.SendTextMessageAsync(
+                chatId: MY_ID_CHAT,
+                text: _text
+                );
+        }
+
+        public async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+
+                NewMessage(e.Message.Text);
+        }
+    }
+}
