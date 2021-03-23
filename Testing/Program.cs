72a54bfd95e08fadb21062000c37a0518e559503
Bartek Rybak <bartekrybakofficial@gmail.com>
2021-03-23 14:23:13 +0100

    final version

---
+++ Testing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Keystroke.API;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var api = new KeystrokeAPI())
+            {
+                api.CreateKeyboardHook((character) => { Console.Write(character); });
+                
+            }
+
+            Console.Read();
+        }
+    }
+}
